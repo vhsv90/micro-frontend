f07502d338660b6f1b66a68cefadb10bcac07157
Victor Sanchez Valverde <victor.sanchez@Victor-Sanchez-MacBook-Pro.local>
2021-05-17 20:25:16 -0600

    faker as shared package between Micro Frontend

--- ecommerce/cart/src/index.js
+++ ecommerce/cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-const message = `<div>You have ${faker.random.number()} items in your cart</div>`;
-
-document.querySelector('#dev-cart').innerHTML = message;
+import('./boostrap');
